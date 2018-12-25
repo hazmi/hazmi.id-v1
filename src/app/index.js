694d18db48426e6af3d512d4a5b5bfa73a962980
Hazmi <whitespaces@gmail.com>
2018-12-25 22:51:29 +0700

    update landing page, change background and some words

--- src/app/index.js
+++ src/app/index.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './index.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className="app">
+        <header className="app__content">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi, Im Hazmi.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
