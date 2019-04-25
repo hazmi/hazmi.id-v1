3484e07f8c0596768b864e3ad7b3fb5fe9ad57e3
Hazmi <whitespaces@gmail.com>
2019-04-25 17:33:15 +0700

    refactor and update todomvc intro text

--- src/app/components/playground/index.js
+++ src/app/components/playground/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './index.scss';
 
+const introTodoMVC = `
+  An example of component based approach of React app. A methodology that 
+  highly concern with the MAINTAINABILITY of a long projects. A clear 
+  separation between logic, styles and assets of each features will make 
+  updating or removing features much more easier to do. Avoid mountains of 
+  dead codes that can cause a significant problems.
+`;
+
 export const Playground = () => (
   <div className="playground">
     <h2>Playground</h2>
@@ -10,7 +18,7 @@ export const Playground = () => (
           <span data-title="TodoMVC">TodoMVC</span>
         </a>
       </h3>
-      <p>An example of component based approach of React app. A methodology that highly concern with the maintainability of a long projects. A clear separation between logic, styles and assets  will make updating or removing features much more easier to do.</p>
+      <p>{introTodoMVC}</p>
       <ul className="playground__item-footer">
         <li>
           <a href="https://todomvc.hazmi.id">
